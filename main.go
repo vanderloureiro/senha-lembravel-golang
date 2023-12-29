3a462f1f7750c153c2513e330e608f757d288b54
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2023-12-29 04:59:50 -0300

    adicao de framework Gin e criacao de rota default

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
 	fmt.Println("Hello World!")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Senha Lembrável API",
+		})
+	})
+	r.Run()
 }
